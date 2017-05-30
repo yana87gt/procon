89b14e0d022bd95a72a14250efd022540c4f1642
t-yana <syts175yana@gmail.com>
2017-05-31 02:15:47 +0900

    remove wpcs2

--- wpcs2/2016/c.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(void){
-    int d;
-    string s[8]={"N","NW","W","SW","S","SE","E","NE"};
-    while(cin>>d,d!=-1)cout<<s[d/45]<<endl;
-    return 0;
-}
