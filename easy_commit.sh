33bb69502280129e3592d14976816cbfb9947aba
Takahiro Yanagawa <syts175yana@gmail.com>
2020-07-26 02:26:52 +0900

    Updated at 2020-07-26(日) 02:26:52

--- easy_commit.sh
+++ easy_commit.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 TMP_NOW=`date '+%F(%a) %T'`
-git add .
+while getopts ":a" optKey; do
+  case "$optKey" in
+    a)
+      git add .
+      ;;
+  esac
+done
 git commit -m "Updated at $TMP_NOW"
 git push origin master
