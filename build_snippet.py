f998c3c9c930112ef185be9fbcbabb7da5dd3954
Takahiro Yanagawa <syts175yana@gmail.com>
2023-06-13 19:01:13 +0900

    Formatting

--- build_snippet.py
+++ build_snippet.py
@@ -1,46 +1,55 @@
-import subprocess
-import re
-import os
 import json
+import os
+import re
+import subprocess
 
-def run(cmd): 
+
+def run(cmd):
     print(cmd)
     subprocess.call(cmd, shell=True)
+
 
 # snippets -> library
 def sublime_to_library():
     sublime_snippets = os.listdir("./snippets")
     for sublime_snippet in sublime_snippets:
-        with open(os.path.join('snippets', sublime_snippet), 'r') as file1:
+        with open(os.path.join("snippets", sublime_snippet), "r") as file1:
             text = file1.read()
-            match_obj = re.match(r'''<snippet>
+            match_obj = re.match(
+                r"""<snippet>
 \s*<content><!\[CDATA\[
 ((.|\n)*)
 \]\]></content>
 \s*<tabTrigger>(.*)</tabTrigger>
 \s*<scope>(.*)</scope>
-</snippet>''', text)
+</snippet>""",
+                text,
+            )
             if match_obj is None:
                 print("Not match!", text)
                 continue
             code, _, tab_trigger, source = match_obj.groups()
-            file_base = re.match(r'(.+)\.sublime-snippet', sublime_snippet).groups()[0]
-            with open(os.path.join('library', file_base + '.cpp'), 'w') as file2:
-                file2.write(f"""///
+            file_base = re.match(r"(.+)\.sublime-snippet", sublime_snippet).groups()[0]
+            with open(os.path.join("library", file_base + ".cpp"), "w") as file2:
+                file2.write(
+                    f"""///
 /// for vscode snippet
 /// prefix: {tab_trigger}
 ///
 
-{code}""")
+{code}"""
+                )
 
-'''
+
+"""
 Code
 -> Preferences
 -> User Snippets
 -> New Snippets file for 'procon' ...
 -> "procon"(.code-snippets) で一旦ファイルを生成(これをしないと認識されない？？)
 -> このスクリプトを走らせる
-'''
+"""
+
 
 # library -> .vscode(snippet)
 def library_to_snippet():
@@ -49,26 +58,30 @@ def library_to_snippet():
     for library in libraries:
         if ".cpp" not in library and ".hpp" not in library:
             continue
-        with open(os.path.join('library', library), 'r') as input_file:
+        with open(os.path.join("library", library), "r") as input_file:
             text = input_file.read()
-        match_obj = re.match(r'''///
+        match_obj = re.match(
+            r"""///
 /// for vscode snippet
 /// prefix:\s*(.+)
 ///
 
-((.|\n)+)''', text)
+((.|\n)+)""",
+            text,
+        )
         if match_obj is None:
             print("Not match!", text)
             continue
         prefix, code, _ = match_obj.groups()
-        file_base, file_extension = re.match(r'(.+)\.(.+)', library).groups()
+        file_base, file_extension = re.match(r"(.+)\.(.+)", library).groups()
         snippet_json[file_base] = {
             "prefix": prefix,
-            "scope": "python" if file_extension == 'py' else file_extension,
-            "body": code.split('\n')
+            "scope": "python" if file_extension == "py" else file_extension,
+            "body": code.split("\n"),
         }
-    with open(os.path.join('.vscode/procon.code-snippets'), 'w') as output_file:
+    with open(os.path.join(".vscode/procon.code-snippets"), "w") as output_file:
         output_file.write(json.dumps(snippet_json, indent=4, ensure_ascii=False))
+
 
 if __name__ == "__main__":
     library_to_snippet()
--- library.cpp
+++ library.cpp
@@ -1,126 +1,137 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for(int i=0;i<int(n);++i)
-#define rep1(i,n) for(int i=1;i<=int(n);++i)
-#define all(a) (a).begin(),(a).end()
-#define each(itr,c) for(auto itr=c.begin(); itr!=c.end(); ++itr)
-#define lb(s,x) lower_bound(all(s),x)-s.begin()
-#define ub(s,x) upper_bound(all(s),x)-s.begin()
-#define uniq(a) a.erase(unique(all(a)),a.end())
-#define debug(x) cerr<<#x<<": "<<x<<endl
+#define rep(i, n) for (int i = 0; i < int(n); ++i)
+#define rep1(i, n) for (int i = 1; i <= int(n); ++i)
+#define all(a) (a).begin(), (a).end()
+#define each(itr, c) for (auto itr = c.begin(); itr != c.end(); ++itr)
+#define lb(s, x) lower_bound(all(s), x) - s.begin()
+#define ub(s, x) upper_bound(all(s), x) - s.begin()
+#define uniq(a) a.erase(unique(all(a)), a.end())
+#define debug(x) cerr << #x << ": " << x << endl
 
-template<class T,class U>
-ostream& operator<<(ostream& o, const pair<T,U> &p){
-    o<<"("<<p.first<<", "<<p.second<<")";
+template <class T, class U>
+ostream &operator<<(ostream &o, const pair<T, U> &p) {
+    o << "(" << p.first << ", " << p.second << ")";
     return o;
 }
 
-template<typename A, size_t N, typename T>
-void Fill(A (&array)[N], const T &val){
-    fill( (T*)array, (T*)(array+N), val );
+template <typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val) {
+    fill((T *)array, (T *)(array + N), val);
 }
 
-#define print(v) { cerr<<#v<<": [ "; for(auto _ : v) cerr<<_<<", "; cerr<<"]"<<endl; }
+#define print(v)                            \
+    {                                       \
+        cerr << #v << ": [ ";               \
+        for (auto _ : v) cerr << _ << ", "; \
+        cerr << "]" << endl;                \
+    }
 
 #define INF 1e9
 typedef long long ll;
 
-using MyClass = pair<int,int>;
+using MyClass = pair<int, int>;
 namespace std {
-    bool operator<(const MyClass a, const MyClass b) {
-        return a.first != b.first ? a.first < b.first : a.second < b.second;
-    }
+bool operator<(const MyClass a, const MyClass b) {
+    return a.first != b.first ? a.first < b.first : a.second < b.second;
+}
+}  // namespace std
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+ll com(ll n, ll r) {
+    if (n < r) return 0;
+    r = min(r, n - r);
+    static vector<vector<ll>> c;
+    if ((ll)c.size() <= n) c.resize(n + 1);
+    if ((ll)c[n].size() <= r) c[n].resize(r + 1);
+    if (c[n][r] != 0)
+        ;
+    else if (r == 0)
+        c[n][r] = 1;
+    else
+        c[n][r] = com(n - 1, r - 1) + com(n - 1, r);
+    return c[n][r];
 }
 
+const ll mod = 1e9 + 7;
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+ll add(ll a, ll b) { return (a + b) % mod; }
 
-ll gcd(ll a,ll b){return b==0 ? a : gcd(b,a%b);}
-ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll sub(ll a, ll b) { return (a - b) % mod; }
 
-ll C[60][60]={};
-ll com(int n, int r){
-    if(C[n][r]!=0) return C[n][r];
-    else if(n==r || r==0) return C[n][r]=1;
-    else if(r==1 || r==n-1) return C[n][r]=n;
-    else return  C[n][r] = C[n][n-r] = com(n-1,r-1) + com(n-1,r);
+ll mul(ll a, ll b) { return (a * b) % mod; }
+
+ll power(ll n, ll r) {
+    if (r == 0) return 1;
+    return mul(power(mul(n, n), r / 2), (r & 1 ? n : 1));
 }
 
-const ll mod = 1e9+7;
-
-ll add(ll a,ll b){ return (a+b)%mod;}
-
-ll sub(ll a,ll b){ return (a-b)%mod;}
-
-ll mul(ll a,ll b){ return (a*b)%mod;}
-
-ll power(ll n, ll r){
-    if(r==0) return 1;
-    return mul(power(mul(n,n),r/2),(r&1 ? n : 1));
-}
-
-ll divm(ll a,ll b){ return mul(a, power(b, mod-2));}
+ll divm(ll a, ll b) { return mul(a, power(b, mod - 2)); }
 
 ll f[200000] = {};
 
-ll fact(ll n){
-    if(f[n]) return f[n];
-    if(n==0) return 1;
-    return f[n] = mul(n,fact(n-1));
+ll fact(ll n) {
+    if (f[n]) return f[n];
+    if (n == 0) return 1;
+    return f[n] = mul(n, fact(n - 1));
 }
 
-ll com(ll n,ll r){
-    if(n<r) return 0;
-    return divm(fact(n),mul(fact(n-r),fact(r)));
+ll com(ll n, ll r) {
+    if (n < r) return 0;
+    return divm(fact(n), mul(fact(n - r), fact(r)));
 }
 
-vector<string> split(string s,string d) {
+vector<string> split(string s, string d) {
     vector<string> elems;
     s += d;
     int r;
-    for(int l=0; l<int(s.size()); l=r+d.size()){
-        r = s.find(d,l);
-        string item = s.substr(l,r-l);
-        if(item!="") elems.push_back(item);
+    for (int l = 0; l < int(s.size()); l = r + d.size()) {
+        r = s.find(d, l);
+        string item = s.substr(l, r - l);
+        if (item != "") elems.push_back(item);
     }
     return elems;
 }
 
-void print_char(){
-    char c=0;
-    rep(i,256){
-        printf("%3d(%2x) : %c\n",i,i,c++);
-        if(c=='\0')printf("it's NULL\n");
+void print_char() {
+    char c = 0;
+    rep(i, 256) {
+        printf("%3d(%2x) : %c\n", i, i, c++);
+        if (c == '\0') printf("it's NULL\n");
     }
 }
 
-void check_sizeof(){
-    printf("sizeof int : %ld\n",sizeof(int));
-    printf("sizeof unsigned int : %ld\n",sizeof(unsigned int));
-    printf("sizeof char : %ld\n",sizeof(char));
-    printf("sizeof short : %ld\n",sizeof(short));
-    printf("sizeof long : %ld\n",sizeof(long));
-    printf("sizeof long long : %ld\n",sizeof(long long));
-    printf("sizeof float : %ld\n",sizeof(float));
-    printf("sizeof double : %ld\n",sizeof(double));
-    printf("sizeof void* : %ld\n",sizeof(void*));
+void check_sizeof() {
+    printf("sizeof int : %ld\n", sizeof(int));
+    printf("sizeof unsigned int : %ld\n", sizeof(unsigned int));
+    printf("sizeof char : %ld\n", sizeof(char));
+    printf("sizeof short : %ld\n", sizeof(short));
+    printf("sizeof long : %ld\n", sizeof(long));
+    printf("sizeof long long : %ld\n", sizeof(long long));
+    printf("sizeof float : %ld\n", sizeof(float));
+    printf("sizeof double : %ld\n", sizeof(double));
+    printf("sizeof void* : %ld\n", sizeof(void *));
 }
 
 /* 10進数(int)を全ビット分,2進数表記で出力 */
-void print_binary(int decimal){
-    unsigned bit_checker = 1u<<((sizeof(int)<<3)-1);   //1000...000
-    do{
-        printf("%d",!!(decimal & bit_checker));
-    }while(bit_checker>>=1);
+void print_binary(int decimal) {
+    unsigned bit_checker = 1u << ((sizeof(int) << 3) - 1);  // 1000...000
+    do {
+        printf("%d", !!(decimal & bit_checker));
+    } while (bit_checker >>= 1);
 }
 
 /* 10進数(int)をnビット分,2進数表記で出力 */
-void print_binary(int decimal, int n){
-    unsigned bit_checker = 1u<<(n-1);   //1000...000
-    do{
-        printf("%d",!!(decimal & bit_checker));
-    }while(bit_checker>>=1);
+void print_binary(int decimal, int n) {
+    unsigned bit_checker = 1u << (n - 1);  // 1000...000
+    do {
+        printf("%d", !!(decimal & bit_checker));
+    } while (bit_checker >>= 1);
 }
 
 union Int_Float {
@@ -129,13 +140,13 @@ union Int_Float {
 };
 
 /* 10進数(float)を全ビット分,2進数表記で出力 */
-void print_binary(float decimal){
-    unsigned bit_checker = 1u<<((sizeof(int)<<3)-1);   //1000...000
+void print_binary(float decimal) {
+    unsigned bit_checker = 1u << ((sizeof(int) << 3) - 1);  // 1000...000
     union Int_Float target;
     target.fval = decimal;
-    do{
-        printf("%d",!!(target.ival & bit_checker));
-    }while(bit_checker>>=1);
+    do {
+        printf("%d", !!(target.ival & bit_checker));
+    } while (bit_checker >>= 1);
 }
 
 union Long_Double {
@@ -144,24 +155,21 @@ union Long_Double {
 };
 
 /* 10進数(double)を全ビット分,2進数表記で出力 */
-void print_binary(double decimal){
+void print_binary(double decimal) {
     union Long_Double target;
     target.dval = decimal;
-    unsigned long long bit_checker = 1ull<<((sizeof(long long)<<3)-1);
-    do{
-        printf("%d",!!(target.llval & bit_checker));
-    }while(bit_checker>>=1);
+    unsigned long long bit_checker = 1ull << ((sizeof(long long) << 3) - 1);
+    do {
+        printf("%d", !!(target.llval & bit_checker));
+    } while (bit_checker >>= 1);
 }
 
-int digits(int n){
-    return to_string(n).size();
-}
-
+int digits(int n) { return to_string(n).size(); }
 
 #define MAX 100
-int main(void){
-    int ary[2*MAX + 1];
-    int *dp = ary + MAX; // [-MAX, MAX]
+int main(void) {
+    int ary[2 * MAX + 1];
+    int *dp = ary + MAX;  // [-MAX, MAX]
     dp[-1] = 3;
     map<string, int> mp;
     mp["hoge"] = 3;
@@ -174,12 +182,12 @@ int main(void){
     st.insert(2.71);
     print(st);
 
-    pair<int,pair<int,int>> p = {3,{1,4}};
-    cout<<p<<endl;
+    pair<int, pair<int, int>> p = {3, {1, 4}};
+    cout << p << endl;
 
     bitset<12> bs1(2214);
     bitset<12> bs2(578);
-    cout<<bitset<12>(12).to_string('X','Y')<<endl;;
+    cout << bitset<12>(12).to_string('X', 'Y') << endl;
 
     string l_r = "RLLLRRRLRRLL";
     debug(bs1);
